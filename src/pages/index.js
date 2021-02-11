24da24bf84bd8c8db8ea948973ad475e9011ee87
ajscholz <yoitsscholzy@gmail.com>
2021-02-11 11:48:57 -0500

    add next steps section to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import HeroBanner from '../components/HeroBanner'
 import { graphql } from 'gatsby'
 import FeaturesSection from '../components/sections/FeaturesSection'
 import CallToActionSection from '../components/sections/CallToActionSection'
-import Layout from '../components/layout'
 import StoriesSection from '../components/sections/StoriesSection'
+import FeaturedCardsSection from '../components/sections/FeaturedCardsSection'
 
 const IndexPage = ({ data }) => {
   return (
@@ -12,11 +12,297 @@ const IndexPage = ({ data }) => {
       <HeroBanner banner={data.page.banner} />
       <FeaturesSection />
       <StoriesSection />
-      <CallToActionSection />
+      {/* <CallToActionSection /> */}
 
-      <GenericSection>Recent Messages and Series</GenericSection>
+      <FeaturedCardsSection />
 
-      <GenericSection>Quick steps</GenericSection>
+      <section className='bg-cn-dk-blue-900'>
+        <div className='pt-12 sm:pt-16 lg:pt-24'>
+          <div className='max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8'>
+            <div className='max-w-3xl mx-auto space-y-2 lg:max-w-none'>
+              <h2 className='text-lg leading-6 font-semibold text-cn-lt-blue-300 uppercase tracking-wider'>
+                Next Steps
+              </h2>
+              <p className='text-3xl font-extrabold text-white sm:text-4xl lg:text-5xl'>
+                Don't wait any more. Take a step today.
+              </p>
+              <p className='text-xl text-cn-lt-blue-300'>
+                Each of our faith journeys is simply a series of steps. No
+                matter where you are on your journey there's a step for you.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='mt-8 pb-12 sm:mt-12 sm:pb-16 lg:mt-16 lg:pb-24'>
+          <div className='relative'>
+            <div className='absolute inset-0 bg-cn-dk-blue-900'></div>
+            <div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+              <div className='max-w-md mx-auto space-y-4 lg:max-w-5xl lg:grid lg:grid-cols-2 lg:gap-5 lg:space-y-0'>
+                <div className='flex flex-col rounded-lg shadow-lg overflow-hidden'>
+                  <div className='px-6 py-8 bg-white sm:p-10 sm:pb-6'>
+                    {/* <div>
+                      <h3
+                        className='inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-indigo-100 text-cn-dk-blue-600'
+                        id='tier-standard'
+                      >
+                        Standard
+                      </h3>
+                    </div> */}
+                    <div className='mt-4 flex items-baseline text-5xl font-extrabold text-cn-dk-blue-800'>
+                      Visit A Service
+                      {/* <span className='ml-1 text-2xl font-medium text-gray-500'>
+                        /mo
+                      </span> */}
+                    </div>
+                    <p className='mt-5 text-lg text-gray-500'>
+                      Come visit on a Sunday. It could change your forever
+                    </p>
+                  </div>
+                  <div className='flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-white space-y-6 sm:p-10 sm:pt-6'>
+                    <ul className='space-y-4'>
+                      <li className='flex items-start'>
+                        <div className='flex-shrink-0'>
+                          {/* <!-- Heroicon name: outline/check --> */}
+                          <svg
+                            className='h-6 w-6 text-green-500'
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                            aria-hidden='true'
+                          >
+                            <path
+                              stroke-linecap='round'
+                              stroke-linejoin='round'
+                              stroke-width='2'
+                              d='M5 13l4 4L19 7'
+                            />
+                          </svg>
+                        </div>
+                        <p className='ml-3 text-base text-gray-700'>
+                          Experience a life-giving service
+                        </p>
+                      </li>
+
+                      <li className='flex items-start'>
+                        <div className='flex-shrink-0'>
+                          {/* <!-- Heroicon name: outline/check --> */}
+                          <svg
+                            className='h-6 w-6 text-green-500'
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                            aria-hidden='true'
+                          >
+                            <path
+                              stroke-linecap='round'
+                              stroke-linejoin='round'
+                              stroke-width='2'
+                              d='M5 13l4 4L19 7'
+                            />
+                          </svg>
+                        </div>
+                        <p className='ml-3 text-base text-gray-700'>
+                          Meet some really great people
+                        </p>
+                      </li>
+
+                      <li className='flex items-start'>
+                        <div className='flex-shrink-0'>
+                          {/* <!-- Heroicon name: outline/check --> */}
+                          <svg
+                            className='h-6 w-6 text-green-500'
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                            aria-hidden='true'
+                          >
+                            <path
+                              stroke-linecap='round'
+                              stroke-linejoin='round'
+                              stroke-width='2'
+                              d='M5 13l4 4L19 7'
+                            />
+                          </svg>
+                        </div>
+                        <p className='ml-3 text-base text-gray-700'>
+                          Something here
+                        </p>
+                      </li>
+
+                      <li className='flex items-start'>
+                        <div className='flex-shrink-0'>
+                          {/* <!-- Heroicon name: outline/check --> */}
+                          <svg
+                            className='h-6 w-6 text-green-500'
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                            aria-hidden='true'
+                          >
+                            <path
+                              stroke-linecap='round'
+                              stroke-linejoin='round'
+                              stroke-width='2'
+                              d='M5 13l4 4L19 7'
+                            />
+                          </svg>
+                        </div>
+                        <p className='ml-3 text-base text-gray-700'>
+                          Something here
+                        </p>
+                      </li>
+                    </ul>
+                    <div className='rounded-md shadow'>
+                      <a
+                        href='#'
+                        className='flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-cn-dk-blue-800 hover:bg-cn-dk-blue-900'
+                        aria-describedby='tier-standard'
+                      >
+                        Get started
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='flex flex-col rounded-lg shadow-lg overflow-hidden'>
+                  <div className='px-6 py-8 bg-white sm:p-10 sm:pb-6'>
+                    {/* <div>
+                      <h3
+                        className='inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-indigo-100 text-cn-dk-blue-600'
+                        id='tier-standard'
+                      >
+                        Enterprise
+                      </h3>
+                    </div> */}
+                    <div className='mt-4 flex items-baseline text-5xl font-extrabold text-cn-dk-blue-800'>
+                      Join A Group
+                      {/* <span className='ml-1 text-2xl font-medium text-gray-500'>
+                        /mo
+                      </span> */}
+                    </div>
+                    <p className='mt-5 text-lg text-gray-500'>
+                      The people in our lives determine the direction and
+                      quality of our lives.
+                    </p>
+                  </div>
+                  <div className='flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-white space-y-6 sm:p-10 sm:pt-6'>
+                    <ul className='space-y-4'>
+                      <li className='flex items-start'>
+                        <div className='flex-shrink-0'>
+                          {/* <!-- Heroicon name: outline/check --> */}
+                          <svg
+                            className='h-6 w-6 text-green-500'
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                            aria-hidden='true'
+                          >
+                            <path
+                              stroke-linecap='round'
+                              stroke-linejoin='round'
+                              stroke-width='2'
+                              d='M5 13l4 4L19 7'
+                            />
+                          </svg>
+                        </div>
+                        <p className='ml-3 text-base text-gray-700'>
+                          Pariatur quod similique
+                        </p>
+                      </li>
+
+                      <li className='flex items-start'>
+                        <div className='flex-shrink-0'>
+                          {/* <!-- Heroicon name: outline/check --> */}
+                          <svg
+                            className='h-6 w-6 text-green-500'
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                            aria-hidden='true'
+                          >
+                            <path
+                              stroke-linecap='round'
+                              stroke-linejoin='round'
+                              stroke-width='2'
+                              d='M5 13l4 4L19 7'
+                            />
+                          </svg>
+                        </div>
+                        <p className='ml-3 text-base text-gray-700'>
+                          Sapiente libero doloribus modi nostrum
+                        </p>
+                      </li>
+
+                      <li className='flex items-start'>
+                        <div className='flex-shrink-0'>
+                          {/* <!-- Heroicon name: outline/check --> */}
+                          <svg
+                            className='h-6 w-6 text-green-500'
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                            aria-hidden='true'
+                          >
+                            <path
+                              stroke-linecap='round'
+                              stroke-linejoin='round'
+                              stroke-width='2'
+                              d='M5 13l4 4L19 7'
+                            />
+                          </svg>
+                        </div>
+                        <p className='ml-3 text-base text-gray-700'>
+                          Vel ipsa esse repudiandae excepturi
+                        </p>
+                      </li>
+
+                      <li className='flex items-start'>
+                        <div className='flex-shrink-0'>
+                          {/* <!-- Heroicon name: outline/check --> */}
+                          <svg
+                            className='h-6 w-6 text-green-500'
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                            aria-hidden='true'
+                          >
+                            <path
+                              stroke-linecap='round'
+                              stroke-linejoin='round'
+                              stroke-width='2'
+                              d='M5 13l4 4L19 7'
+                            />
+                          </svg>
+                        </div>
+                        <p className='ml-3 text-base text-gray-700'>
+                          Itaque cupiditate adipisci quibusdam
+                        </p>
+                      </li>
+                    </ul>
+                    <div className='rounded-md shadow'>
+                      <a
+                        href='#'
+                        className='flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-cn-dk-blue-800 hover:bg-cn-dk-blue-900'
+                        aria-describedby='tier-standard'
+                      >
+                        Get started
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
@@ -32,9 +318,3 @@ export const data = graphql`
     }
   }
 `
-
-const GenericSection = ({ children }) => (
-  <div className='py-20 flex items-center justify-center text-xl border-b-4 border-gray-300'>
-    {children}
-  </div>
-)
